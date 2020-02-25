bcf4493db53dcccc98d512c658255b17dbefcd29
Haresh Vekriya <haresh.vekriya@gmail.com>
2020-02-25 19:15:49 +0000

    re added project with auth

--- src/router.js
+++ src/router.js
@@ -4,75 +4,76 @@ import auth from "@/auth";
 Vue.use(Router);
 
 function loadView(view) {
-  return () =>
-    import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`);
+    return () =>
+        import ( /* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`);
 }
 
 export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: loadView("Home")
-    },
-    {
-      path: "/blog",
-      name: "Blog",
-      component: loadView("Blog")
-    },
-    {
-      path: "/project/:uid",
-      name: "Project",
-      component: loadView("Project"),
-      beforeEnter: requireAuth
-    },
-    {
-      path: "/blog/post/:uid",
-      name: "Post",
-      component: loadView("Post")
-    },
-    {
-      path: "/experience",
-      name: "Experience",
-      component: loadView("Experience")
-    },
-    {
-      path: "/skills",
-      name: "Skills",
-      component: loadView("Skills")
-    },
-    {
-      path: "/login",
-      name: "Login",
-      component: loadView("Login")
-    },
-    {
-      path: "/not-found",
-      name: "not-found",
-      component: loadView("NotFound")
-    },
-    {
-      path: "/preview",
-      name: "Preview",
-      component: loadView("Preview")
-    },
-    {
-      path: "*",
-      redirect: {
-        name: "not-found"
-      }
-    }
-  ]
+    mode: "history",
+    routes: [{
+            path: "/",
+            name: "Home",
+            component: loadView("Home")
+        },
+        {
+            path: "/blog",
+            name: "Blog",
+            component: loadView("Blog")
+        },
+        {
+            path: "/project/:uid",
+            name: "Project",
+            component: loadView("Project"),
+            beforeEnter: requireAuth
+        },
+        {
+            path: "/blog/post/:uid",
+            name: "Post",
+            component: loadView("Post")
+        },
+        {
+            path: "/experience",
+            name: "Experience",
+            component: loadView("Experience")
+        },
+        {
+            path: "/skills",
+            name: "Skills",
+            component: loadView("Skills")
+        },
+        {
+            path: "/login",
+            name: "Login",
+            component: loadView("Login")
+        },
+        {
+            path: "/not-found",
+            name: "not-found",
+            component: loadView("NotFound")
+        },
+        {
+            path: "/preview",
+            name: "Preview",
+            component: loadView("Preview")
+        },
+        {
+            path: "*",
+            redirect: {
+                name: "not-found"
+            }
+        }
+    ]
 });
 
 function requireAuth(to, from, next) {
-  if (!auth.loggedIn() && to.path === "/project/nca") {
-    next({
-      path: "/login",
-      query: { redirect: to.fullPath }
-    });
-  } else {
-    next();
-  }
+    if (!auth.loggedIn() && to.path === "/project/windows-app") {
+        next({
+            path: "/login",
+            query: {
+                redirect: to.fullPath
+            }
+        });
+    } else {
+        next();
+    }
 }
